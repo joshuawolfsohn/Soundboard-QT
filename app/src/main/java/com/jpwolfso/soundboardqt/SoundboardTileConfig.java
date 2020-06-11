4c89c7255323d33bf9937f32f32133b6839288be
Joshua Wolfsohn <jpwolfso@edu.uwaterloo.ca>
2020-06-10 22:51:58 -0400

    basic import and export functionality via native documentpicker intents, increase minimum sdk to 26 to use java.nio.file.Files library for easy file ops

--- app/src/main/java/com/jpwolfso/soundboardqt/SoundboardTileConfig.java
+++ app/src/main/java/com/jpwolfso/soundboardqt/SoundboardTileConfig.java
@@ -1,9 +1,12 @@
 package com.jpwolfso.soundboardqt;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.documentfile.provider.DocumentFile;
 
 import android.Manifest;
 import android.content.ComponentName;
@@ -16,11 +19,15 @@ import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.net.Uri;
 import android.net.rtp.AudioStream;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.FileUtils;
+import android.provider.MediaStore;
 import android.provider.Settings;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -31,14 +38,28 @@ import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.channels.FileChannel;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.FileOwnerAttributeView;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import static android.content.Intent.ACTION_CREATE_DOCUMENT;
+import static android.content.Intent.ACTION_GET_CONTENT;
+import static android.content.Intent.EXTRA_TITLE;
 
 public class SoundboardTileConfig extends AppCompatActivity {
 
@@ -63,7 +84,29 @@ public class SoundboardTileConfig extends AppCompatActivity {
         status = findViewById(R.id.status);
         context = this;
         editText = findViewById(R.id.editText);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() > 0) {
+                    saveButton.setEnabled(true);
+                } else {
+                    saveButton.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
         saveButton = findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(saveRecord);
 
         if (checkSelfPermission(Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO},0);
@@ -86,60 +129,53 @@ public class SoundboardTileConfig extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if (requestCode == 0) {
-            switch (grantResults[0]) {
+        switch (grantResults[0]) {
                 case (PackageManager.PERMISSION_GRANTED): {
+                    if (requestCode == 1) {
+                        exportSound();
+                    } else if (requestCode == 2) {
+                        importSound();
+                    }
                     break;
                 }
                 case (PackageManager.PERMISSION_DENIED): {
-                    AlertDialog dialog = new AlertDialog.Builder(this)
-                            .setMessage("Soundboard Quick Tile requires permission to record audio")
-                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO}, 0);
-                                }
-                            })
+                    AlertDialog.Builder dialog = new AlertDialog.Builder(this)
                             .setNegativeButton("Exit", new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialog, int which) {
                                     finish();
                                 }
                             })
-                            .setCancelable(false)
-                            .create();
-                    dialog.show();
-                    break;
-
-                }
-            }
-        } else if (requestCode == 1) {
-            switch (grantResults[0]) {
-                case (PackageManager.PERMISSION_GRANTED): {
-                    exportSound();
-                    break;
-                }
-                case (PackageManager.PERMISSION_DENIED): {
-                    AlertDialog dialog = new AlertDialog.Builder(this)
-                            .setMessage("Soundboard Quick Tile requires permission to export this button sound to your device storage")
-                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-                                }
-                            })
-                            .setNegativeButton("Exit", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    finish();
-                                }
-                            })
-                            .setCancelable(false)
-                            .create();
+                            .setCancelable(false);
+                    if (requestCode == 0) {
+                        dialog.setMessage("Soundboard Quick Tile requires permission to record audio")
+                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO}, 0);
+                                    }
+                                });
+                    } else if (requestCode == 1) {
+                        dialog.setMessage("Soundboard Quick Tile requires this permission to export this button sound to your device storage")
+                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                                    }
+                                });
+                    } else if (requestCode == 2) {
+                        dialog.setMessage("Soundboard Quick Tile requires this permission to import a sound from your device storage")
+                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+                                    }
+                                });
+                    }
+                    dialog.create();
                     dialog.show();
                     break;
                 }
-            }
         }
     }
 
@@ -159,9 +195,9 @@ public class SoundboardTileConfig extends AppCompatActivity {
 
             mediaRecorder = new MediaRecorder();
             mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT);
             mediaRecorder.setOutputFile(tempfile.getAbsolutePath());
-            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
             try {
                 mediaRecorder.prepare();
             } catch (IOException e) {
@@ -184,30 +220,7 @@ public class SoundboardTileConfig extends AppCompatActivity {
             status.setText("Enter a button title and click the Save button to save the quick tile, or click above to record again");
             recordButton.setOnClickListener(startRecord);
             saveButton.setVisibility(View.VISIBLE);
-            saveButton.setOnClickListener(saveRecord);
             editText.setVisibility(View.VISIBLE);
-            editText.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-                }
-
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
-                    if (s.length() > 0) {
-                        saveButton.setEnabled(true);
-                    } else {
-                        saveButton.setEnabled(false);
-                    }
-                }
-
-                @Override
-                public void afterTextChanged(Editable s) {
-
-                }
-            });
-
-
 
         }
     };
@@ -223,6 +236,7 @@ public class SoundboardTileConfig extends AppCompatActivity {
             }
 
             tempfile.renameTo(file);
+            Toast.makeText(context, "Button saved successfully", Toast.LENGTH_SHORT).show();
 
             finish();
         }
@@ -242,7 +256,14 @@ public class SoundboardTileConfig extends AppCompatActivity {
                 intent.setData(Uri.parse("package:" + getPackageName()));
                 startActivity(intent);
                 break;
-            } case (R.id.export): {
+            } case (R.id.soundimport): {
+                if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+                } else {
+                    importSound();
+                }
+                break;
+            } case (R.id.soundexport): {
                 if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                     requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
                 } else {
@@ -255,22 +276,72 @@ public class SoundboardTileConfig extends AppCompatActivity {
     }
 
     public void exportSound() {
-        try {
-            File exportfile = new File(Environment.getExternalStorageDirectory() + "/soundboardqt", "button" + buttonversion + "-" + new SimpleDateFormat("yyyy-MM-dd").format(new Date()) + ".3gpp");
-            exportfile.getParentFile().mkdirs();
-            exportfile.createNewFile();
+        new AlertDialog.Builder(context)
+                .setTitle("Please read")
+                .setMessage("On the following screen, please browse to the save location and verify the filename ends with the .m4a file extension.")
+                .setPositiveButton("Proceed", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(ACTION_CREATE_DOCUMENT);
+                        intent.setType("audio/*");
+                        intent.putExtra(EXTRA_TITLE, getSharedPreferences("buttons", MODE_PRIVATE).getString("button" + buttonversion, "Button " + buttonversion) + ".m4a");
+                        startActivityForResult(intent, 1);
+                    }
+                }).create().show();
+    }
 
-            FileChannel input = new FileInputStream(file).getChannel();
-            FileChannel output = new FileOutputStream(exportfile).getChannel();
-            input.transferTo(0,input.size(),output);
+    public void importSound() {
 
-            input.close();
-            output.close();
+        new AlertDialog.Builder(context)
+                .setTitle("Please read")
+                .setMessage("On the following screen, please browse and open the file to import.")
+                .setPositiveButton("Proceed", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(ACTION_GET_CONTENT);
+                        intent.setType("audio/*");
+                        startActivityForResult(intent, 0);
+                    }
+                }).create().show();
 
-            Toast.makeText(context, "Button " + buttonversion + " sound backed up to " + file.toString(), Toast.LENGTH_SHORT).show();
+    }
 
-        } catch (IOException e) {
-            Toast.makeText(context, String.valueOf(e), Toast.LENGTH_SHORT).show();
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 0) {
+
+                try {
+                    InputStream inputStream = getContentResolver().openInputStream(data.getData());
+                    Files.copy(inputStream,tempfile.toPath(),StandardCopyOption.REPLACE_EXISTING);
+                    inputStream.close();
+
+                    recordButton.setVisibility(View.GONE);
+                    editText.setVisibility(View.VISIBLE);
+                    saveButton.setVisibility(View.VISIBLE);
+                    status.setText("Enter a button title and click the Save button to finish importing the selected file");
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(context, "Failed to import button sound :(", Toast.LENGTH_SHORT).show();
+                }
+
+            } else if (requestCode == 1) {
+
+                try {
+                    OutputStream outputStream = getContentResolver().openOutputStream(data.getData());
+                    Files.copy(file.toPath(), outputStream);
+                    outputStream.close();
+                    Toast.makeText(context, "Button sound exported successfully", Toast.LENGTH_SHORT).show();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(context, "Failed to export button sound :(", Toast.LENGTH_SHORT).show();
+                }
+
+            }
         }
     }
 }
